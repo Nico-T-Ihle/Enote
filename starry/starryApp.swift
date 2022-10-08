efacb258468da2afe51845db3dabd7cfe7bd04f8
Nico-T-Ihle <n.ihle2003@gmail.com>
2022-10-08 23:04:14 +0200

    Initial Commit

---
+++ starry/starryApp.swift
@@ -0,0 +1,17 @@
+//
+//  starryApp.swift
+//  starry
+//
+//  Created by Nico Ihle on 08.10.22.
+//
+
+import SwiftUI
+
+@main
+struct starryApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
